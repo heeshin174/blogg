25eb62606fec79cae13aab7602c5795e5b688dd5
Heechul Shin <heeshin174@gmail.com>
2025-03-19 21:54:54 +0900

    logout view 구현

--- src/main/java/com/example/demo/controller/UserApiController.java
+++ src/main/java/com/example/demo/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.AddUserRequest;
 import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,5 +20,12 @@ public class UserApiController {
     public String signup(AddUserRequest request) {
         userService.save(request);
         return "redirect:/login"; // 회원 가입 후 로그인 페이지로 리다이렉팅
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
