2344eb98b32e06fd777a217f33289fd1665032f3
Heechul Shin <heeshin174@gmail.com>
2025-03-19 12:37:25 +0900

    블로그 글 전체 조회 Test

--- src/test/java/com/example/demo/controller/BlogApiRestAssuredTest.java
+++ src/test/java/com/example/demo/controller/BlogApiRestAssuredTest.java
@@ -1,11 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.ApiTest;
-import com.example.demo.domain.Article;
 import com.example.demo.dto.AddArticleRequest;
-import com.example.demo.repository.BlogRepository;
 import io.restassured.RestAssured;
-import io.restassured.mapper.ObjectMapper;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
@@ -14,7 +11,7 @@ import org.springframework.http.MediaType;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-class BlogApiControllerTest extends ApiTest {
+class BlogApiRestAssuredTest extends ApiTest {
 
     @Test
     void 아티클추가() {
@@ -28,7 +25,22 @@ class BlogApiControllerTest extends ApiTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    private static AddArticleRequest 아티클추가요청_생성() {
+    @Test
+    void 아티클전체조회() {
+        final AddArticleRequest request = 아티클추가요청_생성();
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request).when().post("/api/articles")
+                .then().log().all().extract();
+
+        // API 요청
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .when().get("/api/articles")
+                .then().log().all().extract();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static AddArticleRequest 아티클추가요청_생성() {
         String title = "새 글";
         String content = "새 글입니다. 잘 부탁해요";
         return new AddArticleRequest(title, content);
